8376127fe58fbb12429798dbd8cccf298c3182e7
TinyAtoms <s.ramsaransingw@gmail.com>
2022-04-16 17:57:23 -0300

    update demo

--- flutter_demos/start_project/lib/main.dart
+++ flutter_demos/start_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 var styles = {
   "normal": TextStyle(),
@@ -88,10 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4,
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
@@ -153,6 +151,28 @@ class AppDrawer extends StatelessWidget {
               },
               child: ListTile(
                 title: Text('members'),
+              ),
+            ),
+            Divider(),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Streams()));
+              },
+              child: ListTile(
+                title: Text('streams'),
+              ),
+            ),
+            Divider(),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => FutureScreen()));
+              },
+              child: ListTile(
+                title: Text('futures'),
               ),
             ),
             Divider(),
@@ -218,46 +238,52 @@ class Layout extends StatelessWidget {
         drawer: AppDrawer(), // the  navigation drawer
         appBar: AppBar(title: Text("layout")), // the title
         body: Center(
-          // the body
+            // the body
             child: Container(
-              child: Column(
+          child: Column(
+            children: [
+              Text("row"),
+              Text("row"),
+              Text("row"),
+              Text("row"),
+              Text("row"),
+              Row(
                 children: [
-                  Text("row"),
-                  Text("row"),
-                  Text("row"),
-                  Text("row"),
-                  Text("row"),
-                  Row(
-                    children: [
-                      Text("column  "),
-                      Text("column "),
-                      Text("column "),
-                      Text("column ")
-                    ],
-                  )
+                  Text("column  "),
+                  Text("column "),
+                  Text("column "),
+                  Text("column ")
                 ],
-              ),
-            )));
+              )
+            ],
+          ),
+        )));
   }
 }
 
 class MemberScreen extends StatelessWidget {
-  final members = [Member("sagar", "Electrotechniek", "Informatica", 2018), Member("sagar", "Electrotechniek", "Informatica", 2018), Member("sagar", "Electrotechniek", "Informatica", 2018)
+  final members = [
+    Member("sagar", "Electrotechniek", "Informatica", 2018),
+    Member("sagar", "Electrotechniek", "Informatica", 2018),
+    Member("sagar", "Electrotechniek", "Informatica", 2018)
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: AppDrawer(), // the  navigation drawer
-      appBar: AppBar(title: Text("layout")), // the title
-      body:   ListView.builder(
-        itemCount: members.length,
-        itemBuilder: (context, i)=>  Card(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Padding(padding: EdgeInsets.all(2),child: Column(children: [
-              Text("I'm ${members[i].naam}, I'm a ${members[i].richting} student who wants to specialize in"
-                  "${members[i].specialisatie}  and  I started in ${members[0].cohort}")])))
-    ));
+        drawer: AppDrawer(), // the  navigation drawer
+        appBar: AppBar(title: Text("layout")), // the title
+        body: ListView.builder(
+            itemCount: members.length,
+            itemBuilder: (context, i) => Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Padding(
+                    padding: EdgeInsets.all(2),
+                    child: Column(children: [
+                      Text(
+                          "I'm ${members[i].naam}, I'm a ${members[i].richting} student who wants to specialize in"
+                          "${members[i].specialisatie}  and  I started in ${members[0].cohort}")
+                    ])))));
   }
 }
 
@@ -268,4 +294,101 @@ class Member {
   int cohort;
 
   Member(this.naam, this.richting, this.specialisatie, this.cohort);
+}
+
+class Event {
+  DateTime time = DateTime.now();
+  String title;
+  String details;
+
+  Event(this.title, this.details) {
+    time = DateTime.now();
+  }
+}
+
+String loremIpsumEnglish =
+    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?";
+var rng = Random();
+
+Stream<Event> eventStreamer() async* {
+  for (int i = 1; i <= 10; i++) {
+    int start = rng.nextInt(loremIpsumEnglish.length - 150);
+    var title = loremIpsumEnglish.substring(start, start + 30);
+    var descr = loremIpsumEnglish.substring(start + 31, start + 150);
+    yield Event(title, descr);
+    await Future.delayed(Duration(seconds: 5));
+  }
+  return;
+}
+
+class Streams extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: AppDrawer(),
+        appBar: AppBar(title: Text("streams")), // the title
+        body: StreamBuilder<Event>(
+            stream: eventStreamer(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Something went wrong');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LinearProgressIndicator();
+              }
+              var event = snapshot.data;
+              return ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) => EventTile(event!));
+            }));
+  }
+}
+
+class EventTile extends StatelessWidget {
+  EventTile(this.e);
+
+  final Event e;
+
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          child: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+            ListTile(
+                title: Text('${e.title} on ${e.time}'),
+                subtitle: Text((e.details.length < 150)
+                    ? '${e.details}'
+                    : e.details.substring(0, 149) + "...")),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+Future<Event> getEvent() async {
+  await Future.delayed(Duration(milliseconds: 200));
+  int start = rng.nextInt(loremIpsumEnglish.length - 150);
+  var title = loremIpsumEnglish.substring(start, start + 30);
+  var descr = loremIpsumEnglish.substring(start + 31, start + 150);
+  return Event(title, descr);
+}
+
+class FutureScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Event>(
+        future: getEvent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) return Text('Error');
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return LinearProgressIndicator();
+          if (!snapshot.hasData || snapshot.data == null)
+            return Text('Role snapshot has no data');
+          final event = snapshot.data;
+          return EventTile(event!);
+        });
+  }
 }
